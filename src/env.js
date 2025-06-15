6759de274f4766b43a67209fb7232bbf8a0ce392
basit-nazir-memon <basitnazir585@gmail.com>
2025-06-15 23:25:05 +0500

    added subscription

--- src/env.js
+++ src/env.js
@@ -1,8 +1,9 @@
 export const envConfig = {
     url: 'http://localhost:3000',
-    backend: 'http://localhost:5000',
+    // backend: 'http://localhost:5000',
     model: 'http://localhost:8000',
     stripePublicKey: 'pk_test_51RGReLDEQxcJeBcpliGsp8FOdXwZ4nhpHg87qU8WX3WLFZG0RazVQ8KB6LOVz5zhvRFUbGEWfKK7t7ZoIlHkvnQ600712Lkh8H', // Replace with your Stripe public key
     // backend: 'https://bidbot-backend.onrender.com'
+    backend: 'https://8fcc-39-43-181-101.ngrok-free.app',
 };
 
